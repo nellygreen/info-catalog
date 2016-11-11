0cbf5085bfd58a2e18af9a7af105057808d9f3f9
nellygreen <nellygreen.cg@gmail.com>
2016-11-11 20:53:16 +0300

    Css-syntax

--- scripts/general.js
+++ scripts/general.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
+	    }, 650, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
